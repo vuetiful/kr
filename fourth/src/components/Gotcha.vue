0191937aaab37a9038e8a777325e17481dca3e2c
kkd927 <kd.kwon@worksmobile.com>
2018-03-30 11:50:07 +0900

    Configure gotcha component layout

--- fourth/src/components/Gotcha.vue
+++ fourth/src/components/Gotcha.vue
@@ -1,11 +1,18 @@
 <template lang="html">
-  <el-container id="gotcha" class="gotcha" direction="vertical" :style="{ width: innerWidth, height: `${innerHeight}px` }">
-    <p class="text">
-      <span class="date">2018년 4월 17일 화요일</span>
-      <span class="time">19:30~21:00</span>
-      <span class="location">구글 캠퍼스 코리아 메인 이벤트 홀</span>
-    </p>
-    <el-button class="register" type="primary">참여 신청 페이지로 가기</el-button>
+  <el-container id="gotcha" class="gotcha" direction="vertical" :style="{ height: `${innerHeight}px` }">
+    <el-carousel :interval="10000" trigger="click" class="bg-carousel">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <div class="bg"></div>
+      </el-carousel-item>
+    </el-carousel>
+    <div class="bg-dot"></div>
+    <div class="text-box" direction="vertical">
+      <p class="date">2018. 04 .17 (화)</p>
+      <p class="time">19:30 ~ 21:00</p>
+      <p class="location">구글 캠퍼스 코리아 메인 이벤트 홀</p>
+      <a class="register">참여 신청</a>
+    </div>
+
     <p class="intro">뷰티풀 코리아는...</p>
   </el-container>
 </template>
@@ -13,9 +20,6 @@
 <script>
 export default {
   computed: {
-    innerWidth () {
-      return window.innerWidth
-    },
     innerHeight () {
       return window.innerHeight
     }
@@ -25,12 +29,91 @@ export default {
 
 <style lang="css" scoped>
 .gotcha {
-  background: url("http://placehold.it/300x250") no-repeat center center, url("http://placehold.it/1440x900") no-repeat center center;
-}
-.text {
   position: relative;
 }
-.button-register {
-  width: 90%;
+.bg-carousel {
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
+.bg {
+  position: absolute;
+  background: url("../assets/img/bg1.jpg") no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 1;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+.bg-dot {
+  position: absolute;
+  background: url("../assets/img/gridtile.png");
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  pointer-events: none;
+}
+.text-box {
+  margin: auto;
+  font-size: 2.5vw;
+  color: white;
+  line-height: 45px;
+  z-index: 10;
+}
+
+.text-box .time {
+  font-size: 1.5vw;
+  line-height: 45px;
+}
+
+.text-box .location {
+  line-height: 70px;
+}
+.register {
+  border: 1px solid #fff;
+  color: #fff;
+  margin-left: auto;
+  margin-right: auto;
+
+  width: 15vw;
+  padding: 1vw 0;
+  font-size: 1.5vw;
+  display: block;
+  margin-top: 50px;
+
+  cursor: pointer;
+}
+
+.register:hover {
+  background-color: white;
+  color: black;
+}
+
+.intro {
+  color: white;
+  z-index: 10;
+  position: absolute;
+  bottom: 100px;
+  left: 50%;
+  width: 240px;
+  margin-left: -120px;
+}
+</style>
+
+<style lang="css">
+  .bg-carousel .el-carousel__container {
+    position: relative;
+    height: 100%;
+  }
+
+  .bg-carousel .el-carousel__indicators {
+    z-index: 5;
+  }
 </style>
--- fourth/src/components/presenters/Presenter.vue
+++ fourth/src/components/presenters/Presenter.vue
@@ -1,11 +1,11 @@
 <template lang="html">
   <el-container class="presenter" direction="vertical">
     <img :src="presenter.imgUrl" alt="프로필 사진" class="image">
-    <p class="text">
+    <div class="text">
       <h3 class="name">{{ presenter.name }}</h3>
       <span class="affiliation">{{ presenter.affiliation }}</span>
       <span class="description">{{ presenter.description }}</span>
-    </p>
+    </div>
     <i class="el-icon-message"></i>
   </el-container>
 </template>
