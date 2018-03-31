a486b7feaad37246b2a9acdae64ccf66d8a68d64
smilee <ideas007@naver.com>
2018-03-31 18:47:45 +0900

    Restyle gotcha
    To improve responsiveness

--- fourth/src/components/Gotcha.vue
+++ fourth/src/components/Gotcha.vue
@@ -4,14 +4,15 @@
       <el-carousel-item v-for="item in 3" :key="item"></el-carousel-item>
     </el-carousel>
     <div class="bg-dot"></div>
-    <div class="text-box">
-      <img src="~@/assets/logo.png" alt="" class="logo" :class="{ desktop }">
-      <p class="date">{{ generalInfo.date }}</p>
-      <p class="time">{{ generalInfo.time}}</p>
-      <p class="location">{{ generalInfo.location }}</p>
-      <el-button type="primary" @click="register" target="_blank" class="register-btn">참가 신청</el-button>
+    <div class="center" :class="{ laptop, desktop }">
+      <img src="~@/assets/logo.png" alt="" class="logo" :class="{ laptop, desktop }">
+      <div class="info" :class="{ laptop, desktop }">
+        <p class="date">{{ generalInfo.date }}<br v-if="mobile || smartphone"><span class="time"> {{ generalInfo.time}}</span></p>
+        <p class="location">{{ generalInfo.location }}</p>
+      </div>
+      <el-button type="primary" :class="{ laptop, desktop }" @click="register" target="_blank" class="register-btn">참가 신청</el-button>
     </div>
-    <p class="intro" v-if="!mobile && !smartphone">{{ generalInfo.intro }}</p>
+    <p class="intro" v-if="laptop || desktop">{{ generalInfo.intro }}</p>
     <svg class="arrows">
       <path class="a" d="M0 0 L15 16 L30 0"></path>
     </svg>
@@ -85,9 +86,9 @@ export default {
   z-index: 2;
   pointer-events: none;
 }
-.text-box {
-  margin: 18% auto 6%;
-  font-size: 1.2em;
+.center {
+  margin-top: 25%;
+  font-size: 1em;
   color: white;
   line-height: 30px;
   /* background: url("~@/assets/img/text_bg.png") no-repeat center center; */
@@ -95,22 +96,31 @@ export default {
   background-blend-mode: overlay;
   z-index: 10;
 }
+.center.laptop {
+  margin-top: 10%;
+}
+.center.desktop {
+  margin-top: 18%;
+}
 .logo {
   width: 14em;
   margin-bottom: 20px;
 }
-.logo.desktop {
+.logo.laptop {
   width: 24em;
 }
-
-.text-box .time {
-  font-size: 0.8em;
-  line-height: 36px;
+.logo.desktop {
+  width: 30em;
 }
 
-.text-box .location {
-  line-height: 50px;
+.info {
+  position: static;
 }
+
+.location {
+  margin-bottom: 20px;
+}
+
 .register-btn {
   margin: 10px 0 30px;
 }
@@ -120,7 +130,7 @@ export default {
   bottom: 100px;
   margin-left: 50%;
   transform: translateX(-50%);
-  width: 40%;
+  width: 60%;
   font-size: 0.75em;
   color: white;
   line-height: 1.2;
